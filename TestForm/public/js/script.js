22fe592dd1906a007900304a7c1ab5a396080645
Cosmicblues <theo.benmoussa@epitech.eu>
2018-08-24 20:40:19 +0200

    fin de la verification du back, front OK

--- TestForm/public/js/script.js
+++ TestForm/public/js/script.js
@@ -1,18 +1,28 @@
 function myFunction() {
     var x = document.getElementById("psw");
-    if (x.type === "password" ) {
+    if (x.type === "password") {
         x.type = "text";
     } else {
         x.type = "password";
     }
 }
 
-function myOtherFunction(){
+function myOtherFunction() {
     var y = document.getElementById("confpsw");
-    if (y.type === "password" ) {
+    if (y.type === "password") {
         y.type = "text";
     } else {
         y.type = "password";
     }
-    
+
+}
+
+function formcheck() {
+    var fields = $(".require")
+        .find("select, textarea, input").serializeArray();
+
+    $.each(fields, function(i, field) {
+        if (!field.value)
+            alert(field.name + ' is required');
+    });
 }
